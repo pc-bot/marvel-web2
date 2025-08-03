89b6d1638feabb38fa4a4e0fcf23a7b33be97e9a
antso <hei.antso.2007@gmail.com>
2025-08-03 15:30:21 +0300

    premieres modifications

--- marvel-characters/src/App.jsx
+++ marvel-characters/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [Characters, setCharacters] = useState([]);
+    const [loading, setLoading] = useState(true)
 
-  return (
-    <>
+    useEffect(() => {
+      const charactersLines = '/characters.json'
+      fetch(charactersLines)
+      .then((res) => {
+        if(!res.ok){
+          throw new Error('erreur HTTP')
+        }
+          return res.json()
+      }).then((data)=>{
+        setCharacters(data.characters || []);
+        setLoading(false)
+        
+      }).catch((err) => {
+        console.error(err)
+        setLoading(false)
+      })
+
+    },[])
+
+      const getRequest = () =>{
+        alert("hello")
+    
+     }
+
+    return (
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className='justify-center flex bg-blue-100 w-200 mx-60 rounded-bl-lg rounded-br-lg'>
+          <button className='bg-yellow-500 shadow-xl w-30 h-10 m-10' onClick={getRequest}>GET</button>
+          <button className='bg-green-500 shadow-xl w-30 m-10'>POST</button>
+          <button className='bg-blue-500 shadow-xl w-30 m-10'>PUT</button>
+          <button className='bg-red-500 shadow-xl w-30 m-10'>DELETE</button>
+        </div>
+        <div className='grid grid-cols-4 grid-rows-2 gap-4'>
+          {loading ? <p>...</p> : Characters.map((char, index) => (
+      <div key={index} className='w-60 h-30 m-5 rounded-sm bg-gray-100 shadow-xl'>
+        <p className='text-center m-2'>{char.id}</p>
+        <h1 className='text-center mt-2'>name : {char.name}</h1>
+        <p className='text-center'>identity : {char.realName}</p>
+        <p className='text-center'>universe : {char.universe}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    ))}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
 }
 
-export default App
+export default App;
