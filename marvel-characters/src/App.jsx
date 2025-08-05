f7bc7da4c5371254babe3e128c232d04d4fa5ca8
antso <hei.antso.2007@gmail.com>
2025-08-05 14:23:09 +0300

    ajout du backend

--- marvel-characters/src/App.jsx
+++ marvel-characters/src/App.jsx
@@ -3,11 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
 function App() {
     const [Characters, setCharacters] = useState([]);
+    const [getcharacterslist, setgetCharacters] = useState(false)
+    const [modal, setModal] = useState(false)
     const [loading, setLoading] = useState(true)
 
-    useEffect(() => {
+    const getRequest = () => {
       const charactersLines = '/characters.json'
       fetch(charactersLines)
       .then((res) => {
@@ -16,7 +20,9 @@ function App() {
         }
           return res.json()
       }).then((data)=>{
-        setCharacters(data.characters || []);
+        setCharacters(data?.characters || []);
+        console.log(data);
+        
         setLoading(false)
         
       }).catch((err) => {
@@ -24,20 +30,27 @@ function App() {
         setLoading(false)
       })
 
+    }
+     
+    useEffect(() => {
+      getRequest();
     },[])
-
-      const getRequest = () =>{
-        alert("hello")
-    
-     }
 
     return (
       <div>
         <div className='justify-center flex bg-blue-100 w-200 mx-60 rounded-bl-lg rounded-br-lg'>
-          <button className='bg-yellow-500 shadow-xl w-30 h-10 m-10' onClick={getRequest}>GET</button>
+          <button className='bg-yellow-500 shadow-xl w-30 h-10 m-10'>GET</button>
           <button className='bg-green-500 shadow-xl w-30 m-10'>POST</button>
           <button className='bg-blue-500 shadow-xl w-30 m-10'>PUT</button>
           <button className='bg-red-500 shadow-xl w-30 m-10'>DELETE</button>
+        </div>
+                  <div className='m-5'>
+        <div className='justify-center flex'>
+        <div className='bg-gray-300 flex flex-row h-10'>
+          <p className='m-3'>id : </p>
+          <input type="text" className='bg-white h-6 m-2 '/>
+          
+        </div>
         </div>
         <div className='grid grid-cols-4 grid-rows-2 gap-4'>
           {loading ? <p>...</p> : Characters.map((char, index) => (
@@ -48,6 +61,8 @@ function App() {
         <p className='text-center'>universe : {char.universe}</p>
       </div>
     ))}
+        </div>
+        <button>close</button>
         </div>
       </div>
     )
