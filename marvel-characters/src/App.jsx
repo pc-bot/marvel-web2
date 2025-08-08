b436075b65eb32c01ea19e058c0102cd8a7a2f94
antso <hei.antso.2007@gmail.com>
2025-08-08 15:42:02 +0300

    ajout du design de app.jsx et de post

--- marvel-characters/src/App.jsx
+++ marvel-characters/src/App.jsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Post from './containers/post.jsx'
 
 function App() {
     const [Characters, setCharacters] = useState([]);
-    const [id, setId] = useState(0);
+    const [openModalPost, setOpenModalPost] = useState(false);
     const [loading, setLoading] = useState(true);
 
     const getRequest = async () => {
@@ -34,15 +35,21 @@ function App() {
 
     return (
         <div className="App">
-          <div></div>
-          <div className='bg-white grid grid-cols-4 gap-4 p-4'>{Characters.map((list) => (
-            <div key={list.id} className='border p-4 rounded shadow'>
-              <li>name : {list.name}</li>
-              <li>id : {list.id}</li>
-              <li>identité : {list.realName}</li>
-              <li>universe : {list.universe}</li>
+          <div className='bg-gray-800 text-white p-4 flex justify-between items-center'>
+            <button onClick={() => setOpenModalPost(true)}>post</button>
+
+          </div>
+          <div className='bg-white mt-30 h-100 grid grid-cols-4 gap-4 p-4'>{Characters.map((list) => (
+            <div key={list.id} className=' shadow-lg p-4 rounded-lg bg-gray-100'>
+              <p className='text-center'>{list.id}</p>
+              <h2 className='text-center text-blue-900 font-bold'>{list.name}</h2>
+              <p className='text-center mt-3'>name : {list.name}</p>
+              <p className='text-center'>identité : {list.realName}</p>
+              <p className='text-center'>universe : {list.universe}</p>
             </div>
           ))}</div>
+          {loading && <div className='text-center'>Loading...</div>}
+          {openModalPost && <Post closeModalPost={() => setOpenModalPost(false)} />}
         </div>
     );
     
---
+++ marvel-characters/src/containers/post.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Post({closeModalPost}) {
+    return(
+        <div className="flex items-center justify-center fixed inset-0  bg-opacity-30">
+            <div className="bg-white p-6 rounded-lg shadow-lg w-100 h-90"> 
+            <h1 className="text-center text-2xl font-bold text-indigo-900">Add a new Character</h1>
+            <form className="m-5">
+                <div className="flex flex-col">
+                <label className="text-blue-900">Name:</label>
+                <input type="text" name="name" className="bg-gray-100 border border-gray-300" required />
+                <br />
+                </div>
+                <div className="flex flex-col mt-3">
+                <label className="text-blue-900">Real Name:</label>           
+                <input type="text" name="realName" className="bg-gray-100 border border-gray-300" required />
+                <br />
+                </div>
+                <div className="flex flex-col mt-3">
+                <label className="text-blue-900">Universe:</label>        
+                <input type="text" name="universe" className="bg-gray-100 border border-gray-300 " required />
+                <br />
+                </div>
+                <div className="flex justify-center items-center mt-7">
+                    <button type="submit" className="bg-green-500 rounded-md w-20 h-15 text-white">Add Character</button>
+                    <button onClick={closeModalPost} className="bg-red-500 rounded-md w-20 h-15 text-white mx-10">Cancel</button>
+                </div>
+            </form>
+            </div>
+        </div>
+    )
+}
