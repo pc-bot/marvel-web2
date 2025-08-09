2f24b4a0c01de6c1b3c22f103b3d79b9efe84353
antso <hei.antso.2007@gmail.com>
2025-08-09 07:49:17 +0300

    amélioration de delete

--- marvel-characters/src/containers/delete.jsx
+++ marvel-characters/src/containers/delete.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Delete({closeModalDelete}, refreshCharacters) {
+export default function Delete({closeModalDelete, refreshCharacters}) {
     const [id, setId] = React.useState('');
     const [error, setError] = React.useState(null);
     const [character, setCharacter] = React.useState(null);
@@ -48,7 +48,7 @@ export default function Delete({closeModalDelete}, refreshCharacters) {
 
     return (
         <div className="flex items-center justify-center fixed inset-0 bg-opacity-30">
-                <div className="bg-white p-6 rounded-lg shadow-lg w-120 h-50">
+                <div className="bg-white p-6 rounded-lg shadow-lg w-120 h-80">
                     <h2 className="text-lg font-semibold text-center text-indigo-900">Delete a character</h2>
                     <div className="flex flex-row m-5 align-center justify-center bg-gray-100 border border-gray-300 rounded-md p-3">
                         <h1 className="mx-5">id :</h1>
